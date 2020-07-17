402d371dfdf8bbccb4326611f33f79073acb15cd
Paulo Sales <salespaulo08gmail.com>
2020-07-17 16:08:18 -0300

    Implementacao dos campos na entidade Estudante e testes unitarios

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -1,5 +1,67 @@
 package br.com.tokiomarine.seguradora.avaliacao.entidade;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "estudante")
 public class Estudante {
-	// TODO Implementar a entidade Estudante conforme solicitado
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Integer id;
+
+	@NotNull(message = "Nome é obrigatório!")
+	@Column(nullable = false, length = 100)
+	private String nome;
+
+	@NotNull(message = "E-mail é obrigatório!")
+	@Email(message = "E-mail é invalido!")
+	@Column(nullable = false, length = 200)
+	private String email;
+
+	@Column(nullable = false, length = 20)
+	private String telefone;
+
+	public Integer getId() {
+		return id;
+	}
+
+	protected void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+
 }
